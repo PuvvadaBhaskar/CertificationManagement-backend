3c973aa4c9f3823f02764f3cf5985e425d0b9dae
PUVVADA BHASKAR <2400030295@kluniversity.in>
2026-04-28 16:25:28 +0530

    added cors

--- src/main/java/com/project/config/CorsConfig.java
+++ src/main/java/com/project/config/CorsConfig.java
@@ -1,17 +1,15 @@
 package com.project.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOriginPatterns("http://localhost:5173") // 🔥 CHANGE THIS
+                .allowedOriginPatterns("*") // ✅ allow all origins
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
